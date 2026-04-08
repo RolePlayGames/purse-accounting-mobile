b2db7ad41d5a9711db8c660b5250c75c56be7c49
Александр Плеханов <Plehalex09@gmail.com>
2026-04-09 01:11:01 +0300

    [PA-614] - Transactions tab was moved

---
+++ PurseAccounting.Mobile.Presentation/Pages/Authorized/Transactions/TransactionsTab.xaml.cs
@@ -0,0 +1,9 @@
+namespace PurseAccountinng.Mobile.Presentation.Pages.Authorized.Transactions;
+
+public partial class TransactionsTab : ContentView
+{
+	public TransactionsTab()
+	{
+		InitializeComponent();
+	}
+}
--- PurseAccounting.Mobile.Presentation/Pages/Authorized/TransactionsTab.xaml.cs
+++
@@ -1,9 +0,0 @@
-namespace PurseAccountinng.Mobile.Presentation.Pages.Authorized;
-
-public partial class TransactionsTab : ContentView
-{
-    public TransactionsTab()
-    {
-        InitializeComponent();
-    }
-}
