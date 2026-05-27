d81a6675e5f3ecc8a3e0b0cd851ffeabf92d5a47
Александр Плеханов <Plehalex09@gmail.com>
2026-05-28 02:30:58 +0300

    [PA-633] - DistributionTab intendents were fixed

--- PurseAccounting.Mobile.Presentation/Pages/Authorized/Distribution/DistributionTab.xaml.cs
+++ PurseAccounting.Mobile.Presentation/Pages/Authorized/Distribution/DistributionTab.xaml.cs
@@ -2,11 +2,11 @@ namespace PurseAccountinng.Mobile.Presentation.Pages.Authorized.Distribution;
 
 public partial class DistributionTab : ContentView
 {
-	public DistributionTab(IServiceProvider serviceProvider)
-	{
-		InitializeComponent();
+    public DistributionTab(IServiceProvider serviceProvider)
+    {
+        InitializeComponent();
 
-		var distributionTabViewModel = ActivatorUtilities.CreateInstance<DistributionTabViewModel>(serviceProvider);
-		BindingContext = distributionTabViewModel;
-	}
+        var distributionTabViewModel = ActivatorUtilities.CreateInstance<DistributionTabViewModel>(serviceProvider);
+        BindingContext = distributionTabViewModel;
+    }
 }
