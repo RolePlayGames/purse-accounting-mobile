25f0932cf15d7a3e068f1a70171329813273937e
Александр Плеханов <Plehalex09@gmail.com>
2026-09-04 20:37:09 +0300

    [PA-609] - AutoPlannedSettingIcon was added

---
+++ PurseAccounting.Mobile.Presentation/Components/Icons/AutoPlannedSettingIcon.xaml.cs
@@ -0,0 +1,9 @@
+namespace PurseAccountinng.Mobile.Presentation.Components.Icons;
+
+public partial class AutoPlannedSettingIcon : ContentView
+{
+	public AutoPlannedSettingIcon()
+	{
+		InitializeComponent();
+	}
+}
